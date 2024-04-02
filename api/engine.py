41357128d3ce75c2066103d40335172bdd77ddfa
fdemian <fdemian@gmail.com>
2024-04-02 13:03:05 -0300

    Fix ban tests.

--- api/engine.py
+++ api/engine.py
@@ -2,7 +2,15 @@ from sqlalchemy.engine import Engine
 
 
 def get_engine_from_context(info) -> Engine:
-    return info.context["request"].app.state.engine
+    info_type = str(type(info))
+    if info_type == "<class 'graphql.type.definition.GraphQLResolveInfo'>":
+        return info.context["request"].app.state.engine
+    else:
+        if info_type == "<class 'starlette.requests.Request'>":
+            return info.app.state.engine
+        else:
+            return info.context["request"].app.state.engine
+
 
 
 def get_engine_from_request(request) -> Engine:
